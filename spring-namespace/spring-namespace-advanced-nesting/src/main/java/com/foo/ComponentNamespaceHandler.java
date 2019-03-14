488f3c4e034475cac071c89d731f2f6f01718761
wangsaichao <wangsaichao@ruubypay.com>
2019-03-14 12:16:05 +0800

    spring命名空间元素嵌套使用

---
+++ spring-namespace/spring-namespace-advanced-nesting/src/main/java/com/foo/ComponentNamespaceHandler.java
@@ -0,0 +1,12 @@
+package com.foo;
+
+import org.springframework.beans.factory.xml.NamespaceHandlerSupport;
+
+public class ComponentNamespaceHandler extends NamespaceHandlerSupport {
+
+    @Override
+    public void init() {
+        registerBeanDefinitionParser("component", new ComponentBeanDefinitionParser());
+    }
+
+}
